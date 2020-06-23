319568056bd891a4ed7260d46f91ed492050a00c
bradleypaulread <bradleypaulread@gmail.com>
2020-06-23 21:17:53 +0100

    Implemented chained spider calls and final sire database generation

--- racingpost_scraper/main.py
+++ racingpost_scraper/main.py
@@ -3,10 +3,12 @@ import sqlite3
 from racingpost_scraper.pipelines import DATABASE
 from racingpost_scraper.spiders.race_spider import RaceSpider
 from racingpost_scraper.spiders.racecard_spider import RaceCardSpider
+from racingpost_scraper.spiders.horse_spider import HorseSpider
 from scrapy.crawler import CrawlerProcess, CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 from twisted.internet import reactor, defer
+from scripts.progeny_stats import generate_sire_table
 
 configure_logging()
 runner = CrawlerRunner()
@@ -15,6 +17,7 @@ runner = CrawlerRunner()
 crawlers = [
     (RaceSpider, "Races", "url"),
     (RaceCardSpider, "Race_Runners", "horse_url"),
+    (HorseSpider, "Horses", "tmp"),
 ]
 
 
@@ -40,12 +43,29 @@ def crawl():
 def main():
     crawl()
     reactor.run()
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT sire, sire_url, dam_sire, dam_sire_url FROM Horses")
+    rows = cursor.fetchall()
+    horses = []
+    for row in rows:
+        horses.append((row[0], row[1]))
+        horses.append((row[2], row[3]))
+    generate_sire_table(horses)
+
+    print("JOB COMPLETE!")
 
 
 def main2():
     process = CrawlerProcess(get_project_settings())
 
-    process.crawl("race-spider")
+    connection = sqlite3.connect(DATABASE)
+    cursor = connection.cursor()
+    url_query = "SELECT horse_url FROM Race_Runners"
+    start_urls = [t[0] for t in cursor.execute(url_query)]
+    connection.close()
+
+    process.crawl("horse-spider", start_urls=start_urls)
     process.start()
 
 
