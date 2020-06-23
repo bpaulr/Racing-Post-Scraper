0011200ab3e354ca69a2c9b51d11bde77142512e
bradleypaulread <bradleypaulread@gmail.com>
2020-06-23 21:16:40 +0100

    Implemented optional use of row_hash for primary key

--- racingpost_scraper/racingpost_scraper/pipelines.py
+++ racingpost_scraper/racingpost_scraper/pipelines.py
@@ -24,31 +24,34 @@ def sort_dict(d: dict) -> dict:
 
 def generate_table_statement(table_name: str, pk_name: str, item: dict) -> str:
     return f"CREATE TABLE IF NOT EXISTS {table_name} ({pk_name} " + \
-           f"TEXT PRIMARY KEY, {', '.join([str(k) + ' TEXT' for k in item.keys()])})"
+           f"TEXT PRIMARY KEY, {', '.join([str(k) + ' TEXT' for k in item.keys() if str(k) != pk_name])})"
 
 
-def generate_insert_statement(table: str, item: dict, pk_name: str) -> str:
-    keys, values, statement_vals = [], [], []
+def generate_insert_statement(table: str, item: dict, pk_name: str, use_hash: bool) -> str:
+    keys, values = [], []
     for k, v in item.items():
-        keys.append(k)
+        if k != pk_name:
+            keys.append(k)
         values.append("?")
-    values.append("?")  # extra one for primary key
+    if use_hash:
+        values.append("?")  # extra one for hash primary key
     keys_str = ', '.join(keys)
     values_str = ', '.join(values)
     return f"INSERT INTO {table} ({pk_name + ', ' + keys_str}) VALUES ({values_str})"
 
 
 class UtilPipeline:
-    def __init__(self, table: str, pk_name: str):
+    def __init__(self, table: str, pk_name: str, use_hash=False):
         self.table = table
         self.pk_name = pk_name
         self.connection = sqlite3.connect(DATABASE)
+        self.use_hash = use_hash
         self.cursor = self.connection.cursor()
         self.cursor.execute(f"SELECT name FROM sqlite_master WHERE type='table' AND name='{self.table}'")
         self.table_exists = len(self.cursor.fetchall()) == 1
 
     def process_item(self, item):
-        sorted_dict = sort_dict(item)
+        item_dict = sort_dict(item) if self.use_hash else item.deepcopy()
 
         if not self.table_exists:
             create_statement = generate_table_statement(self.table, self.pk_name, item)
@@ -57,11 +60,15 @@ class UtilPipeline:
             self.table_exists = True
 
         try:
-            insert_statement = generate_insert_statement(self.table, sorted_dict, self.pk_name)
-            values = [hash_dict(sorted_dict)] + list(sorted_dict.values())
+            insert_statement = generate_insert_statement(self.table, item_dict, self.pk_name, self.use_hash)
+            values = None
+            if self.use_hash:
+                values = [hash_dict(item_dict)] + list(item_dict.values())
+            else:
+                values = list(item_dict.values())
             self.cursor.execute(insert_statement, values)
         except sqlite3.IntegrityError:
-            pass
+            pass  # means item is already in database
         except sqlite3.OperationalError:
             print(item)
 
@@ -72,7 +79,7 @@ class UtilPipeline:
 
 class RaceCardItemPipeline:
     def open_spider(self, spider):
-        self.pipeline = UtilPipeline("Races", "race_id")
+        self.pipeline = UtilPipeline("Races", "race_id", use_hash=True)
 
     def close_spider(self, spider):
         self.pipeline.close()
